c9d3d24840a74e45b37e2a9a8540cbf5e4c95854
zchiodin <zchiodin@cisco.com>
2022-11-13 13:39:23 -0500

    Getting started

---
+++ objects.py
@@ -0,0 +1,45 @@
+class Object3D:
+
+    def __init__(self):
+        pass
+
+
+class Ovoid(Object3D):
+
+    def __init__(self):
+        pass
+
+
+class Sphere(Ovoid):
+
+    def __init__(self):
+        pass
+
+
+class Disk(Object3D):
+    
+    def __init__(self):
+        pass
+
+
+class Cuboid(Object3D):
+
+    def __init__(self):
+        pass
+
+
+class Cube(Cuboid):
+
+    def __init__(self):
+        pass
+
+
+class Square(Object3D):
+    def __init__(self):
+        pass
+
+
+class Cylinder(Object3D):
+
+    def __init__(self):
+        pass
