7ea9ec14cafdc64b5c232038a94efa37e9772a48
zchiodin <zchiodin@cisco.com>
2022-11-13 13:24:10 -0500

    Getting started

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-import numpy
-import pygame
+from numpy import array, int8
+from pygame import display, init
+from pygame.time import Clock
+
+
+class Render:
+    
+    def __init__(self, frame_rate: int8 = int8(0), width: int8 = int8(0), height: int8 = int8(0)):
+        self.frame_rate = frame_rate
+        self.resolution = array([width, height], dtype=int8)
+        self.display = display.set_mode(self.resolution)
+        self.clock = Clock()
+        init()
 
 
 if __name__ == '__main__':
+    Render()
     pass
